61ea2c26b66399ae6b32d354112ca4c95fe3af6c
pavell_chip <pavellchip@gmail.com>
2020-07-19 17:28:59 +0300

    Create project about Restaurant . On site is header with menu , menuu is animated on js . Page has media and menu

---
+++ script.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector('.menu');
+console.log(menu);
+let idMenWin = document.querySelector('.menu-win');
+let idLogo = document.querySelector('.logo');
+let idMenImg = document.querySelector('.men-logo');
+var handleMatchMedia = function (mediaQuery) {
+        if (mediaQuery.matches) {
+          menu.classList.add('min-menu');
+          idLogo.classList.remove('d-none');
+          idMenImg.classList.remove('d-none');
+        }else{
+          menu.classList.remove('min-menu');
+          idLogo.classList.add('d-none');
+          idMenImg.classList.add('d-none');
+          idMenWin.classList.add('d-none');
+        }
+    },
+    mql = window.matchMedia('all and (max-width: 926px)');
+
+handleMatchMedia(mql);
+mql.addListener(handleMatchMedia);
+
+/*show window menu*/
+
+let showMen =()=>{
+  idMenWin.classList.toggle('d-none');
+}
+idMenImg.addEventListener('click',showMen);
