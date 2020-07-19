a887fe78e145c7b9c1ffb33ca7d6f3e43934ed10
pavell_chip <pavellchip@gmail.com>
2020-07-20 02:23:06 +0300

    Created slider with js , dinamic slider ,three slides. js slider not good.But all working

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let menu = document.querySelector('.menu');
-console.log(menu);
 let idMenWin = document.querySelector('.menu-win');
 let idLogo = document.querySelector('.logo');
 let idMenImg = document.querySelector('.men-logo');
@@ -27,4 +26,50 @@ let showMen =()=>{
 }
 idMenImg.addEventListener('click',showMen);
 
-/*blocks*/
+/*blocks Slide*/
+
+
+let getButtSlide = document.querySelectorAll('.one-butt-slide');
+let btn_slide = document.getElementById('btn-slide-block');
+let buttWithActive = document.querySelector('.slide-butt-active');
+
+// btn_slide.addEventListener('click', function(event) {
+//   var index = getButtSlide.indexOf(event.target);
+//
+//    if (~index) {
+//        return console.log(index);
+//    }
+// });
+
+getButtSlide.forEach(function(but, i) {
+    but.addEventListener("click", function() {
+      /*Смена активности кнопок*/
+      let buttWithActive = document.querySelector('.slide-butt-active');
+        buttWithActive.classList.remove('slide-butt-active');
+        getButtSlide[i].classList.add('slide-butt-active');
+
+        /*let getBlockSlide = document.querySelector('#slide-id .flexDiv');
+        getBlockSlide.classList.add('d-none');
+        getBlockSlide = document.querySelectorAll('#slide-id .flexDiv.d-none');
+        getBlockSlide[i].classList.remove('d-none');
+
+        Почему-то эта часть кода работала неправильно и в 3 часа ночи я решил пойти по не такому умному решению , а по противоположному решению такому как код который ниже
+        */
+        let getBlockSlide = document.querySelectorAll('#slide-id .flexDiv');
+        if(i == 0){
+          getBlockSlide[0].classList.remove('d-none');
+          getBlockSlide[1].classList.add('d-none');
+          getBlockSlide[2].classList.add('d-none');
+        }else if(i==1){
+          getBlockSlide[0].classList.add('d-none');
+          getBlockSlide[1].classList.remove('d-none');
+          getBlockSlide[2].classList.add('d-none');
+        }else if(i == 2){
+          getBlockSlide[0].classList.add('d-none');
+          getBlockSlide[1].classList.add('d-none');
+          getBlockSlide[2].classList.remove('d-none');
+        }else{
+          alert('oops');
+        }
+    });
+});
