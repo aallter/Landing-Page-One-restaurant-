fde39d3b39eb8f2d5603570f3cee4ef073295b85
pavell_chip <pavellchip@gmail.com>
2020-07-21 10:26:00 +0300

    Create ancors link to blocks with js

--- script.js
+++ script.js
@@ -78,3 +78,19 @@ for (let i = 0; i < getCategoriyMenu.length; i++) {
     getImgMenu[ind].classList.add('my-d-block');
  });
 }
+
+/*Плавный переход по якорным ссылкам*/
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start', inline: 'start'
+    })
+  })
+}
